af450c11f603a5405628e949f7029434aaa44790
OsamaWebArtist <145370726+OsamaWebArtist@users.noreply.github.com>
2024-03-13 11:57:13 +0000

    add files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!--eslint-disable-->
+<template>
+  <appView />
+  <router-view />
+</template>
+
+<script>
+/*eslint-disable*/
+import appView from "./views/appView.vue";
+
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    "appView": appView,
+
+  }
+}
+</script>
